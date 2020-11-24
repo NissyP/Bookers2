381637ecc1d2939979b074c03cc4371b893da82c
NissyP <ishika.12.twelve@gmail.com>
2020-11-24 11:07:57 +0000

    [Add] 20:07

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to books_path
+      redirect_to book_path(@book.id)
     else
       @books = Book.page(params[:page]).reverse_order
       @user = current_user
